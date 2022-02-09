10b62d5ab268acd0472fff7c431679a878fa02d2
AkasDutta <94923887+AkasDutta@users.noreply.github.com>
2022-02-09 14:59:20 +0530

    Update __init__.py

--- veros_bgc/__init__.py
+++ veros_bgc/__init__.py
@@ -9,18 +9,17 @@ from ._version import get_versions
 __version__ = get_versions()['version']
 del get_versions
 
-from veros_bgc.variables import MAIN_VARIABLES, CONDITIONAL_VARIABLES
+from veros_bgc.variables import VARIABLES
 from veros_bgc.settings import SETTINGS
 from veros_bgc.core.npzd import npzd, setupNPZD
 from veros_bgc.diagnostics.npzd_monitor import NPZDMonitor
 
 
-__VEROS_INTERFACE__ = dict(
+__VEROS_INTERFACE__ = {
     name='biogeochemistry',
     setup_entrypoint=setupNPZD,
     run_entrypoint=npzd,
     settings=SETTINGS,
     variables=MAIN_VARIABLES,
-    conditional_variables=CONDITIONAL_VARIABLES,
     diagnostics=[NPZDMonitor],
-)
+}
