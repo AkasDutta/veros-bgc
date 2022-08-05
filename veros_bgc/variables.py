5e86627c2efb89bb5bc9684dd4abddcb9284fe8c
AkasDutta <akash.dutta5061@gmail.com>
2022-08-05 16:06:29 +0530

    Update variables.py

--- veros_bgc/variables.py
+++ veros_bgc/variables.py
@@ -15,7 +15,7 @@ VARIABLES = {
     ),
     "bgc_tracers": Variable(
         "All tracers",
-        T_GRID + TIMESTEPS + ("bgc_tracers_idx"),
+        T_GRID + TIMESTEPS + ("bgc_tracers_idx",),
         "variable",
         "5D array of many 4D tracers; variable size of 5th Dimension",
         write_to_restart=True,
